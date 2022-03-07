6a1be742dc774d09651383ad536db446a79f9817
Vivek Kumar <vivek32032@gmail.com>
2022-03-07 20:40:54 +0530

    add server.js file

---
+++ block-BNaabz/sample/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var logger = require('morgan')
+var mongoose = require('mongoose')
+var app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb/sample",(err)=>{
+    console.log(err ? err : 'connected to database')
+})
+
+ 
+app.get('/',(req,res)=>{
+    res.send('welcome')
+});
+
+app.use((req,res,next)=>{
+    res.send('Page not found')
+});
+
+app.listen(4444,()=>{
+    console.log('server is listening on port 4444');
+});
