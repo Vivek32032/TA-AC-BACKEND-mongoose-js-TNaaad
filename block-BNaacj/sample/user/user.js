07fd0b67afe3258a514908e0e590df2d2fb3e9c5
Vivek Kumar <vivek32032@gmail.com>
2022-03-10 10:12:23 +0530

    refactor

--- block-BNaacj/sample/user/user.js
+++ block-BNaacj/sample/user/user.js
@@ -3,8 +3,12 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var userLoginInfo = new Schema({
-    
+    name : String,
+    email : {type :String, lowercase :true},
+    age: {type : Number, default : 0},
+    favorites : [String],
+    marks : [Number],
     username : {type : String, required : true},
-    password : {type : String, minLenth : 5, maxLength : 20},
-    createdAt : {type : Date, default : (new Date()).getDate()}
+    password : {type : String, minLenth : 5, maxLength : 15},
+    createdAt : {type : Date, default : new Date()}
 });
